bdb393f6f453f7343c8d25794e08165ec74f45ba
Edgar Martinez <50941094+depinore2@users.noreply.github.com>
2022-02-15 13:05:46 -0800

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -2,7 +2,7 @@
 
 # NOTE: 
 #   The kind cluster assumes that ports 80 and 443 are available on your host machine.
-#   If this is not the case, update lines 7 and 10 of /automation/kind.yaml
+#   If this is not the case, update lines 7 and 10 of /_scripts/kind.yaml
 
 & $psscriptroot/_scripts/init-kind.ps1 # initialize kind if it's not already
 get-childitem deploy.ps1 -recurse | % { & $_.fullname } # run every deploy script in the repo
